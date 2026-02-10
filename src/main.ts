366acfc0bf73653605c5d1123f5adf6653d9e1d9
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-02-10 17:37:50 -0300

    Alteração para usar Fastify ao invés do Express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
   app.enableShutdownHooks()
-  await app.listen(process.env.PORT ?? 3333)
+  await app.listen(process.env.PORT ?? 3333, '0.0.0.0')
 }
 bootstrap()
